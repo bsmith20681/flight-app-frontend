e1973bbd7334d55484db650a1f51d975c4b13b79
Ben <bsmith20681@gmail.com>
2021-04-27 20:25:40 -0600

    added fetch request for skyscanner api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState("");
+  const options = {
+    method: "GET",
+    url:
+      "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/LAS-sky/MCI-sky/anytime",
+    params: { inboundpartialdate: "anytime" },
+    headers: {
+      "x-rapidapi-key": "6548d07ae3msh59978a08b6317aep162abajsnf71503eccb2d",
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <h1>This is the home page</h1>
