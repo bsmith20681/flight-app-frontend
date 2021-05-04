b7ae03bde7d32f926281c354d35e342f9fd88904
Ben <bsmith20681@gmail.com>
2021-05-03 20:11:21 -0600

    added months to calendar

--- src/components/SelectDate.js
+++ src/components/SelectDate.js
@@ -5,6 +5,7 @@ import angleRight from "../images/angle-right.svg";
 
 const SelectDate = () => {
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [month, setMonth] = useState(0);
   const toggleCalendar = () => {
     setIsCalendarOpen(!isCalendarOpen);
   };
@@ -25,17 +26,71 @@ const SelectDate = () => {
     };
   });
 
+  const currentMonth = new Date().getMonth() - 1;
+
+  const createMonths = Array.from({ length: 12 }, (item, i) => {
+    return new Date(0, currentMonth - i).toLocaleString("en-US", {
+      month: "long",
+    });
+  }).reverse();
+
+  const nextMonth = () => {
+    setMonth(month + 1);
+  };
+
+  const prevMonth = () => {
+    setMonth(month - 1);
+  };
+
   return (
-    <div ref={detectOutsideClick} className="selectDate">
-      <div onClick={toggleCalendar}>
-        <img className="selectDate-calendar" src={calendar} alt="calendar" />
+    <>
+      <div ref={detectOutsideClick} className="selectDate">
+        <div onClick={toggleCalendar}>
+          <img className="selectDate-calendar" src={calendar} alt="calendar" />
+        </div>
+        <div
+          className={`selectDate-popup ${
+            isCalendarOpen ? "visible" : "hidden"
+          }`}
+        >
+          <div className="selectDate-month_container">
+            {month <= 0 ? (
+              <img
+                className="selectDate-arrows_unactive"
+                src={angleLeft}
+                alt="angle left"
+              />
+            ) : (
+              <img
+                onClick={prevMonth}
+                className="selectDate-arrows"
+                src={angleLeft}
+                alt="angle left"
+              />
+            )}
+
+            <div className="selectDate-month">
+              <p>{createMonths[month]}</p>
+              <p>{createMonths[month + 1]}</p>
+            </div>
+            {month >= 11 ? (
+              <img
+                className="selectDate-arrows_unactive"
+                src={angleRight}
+                alt="angle right"
+              />
+            ) : (
+              <img
+                onClick={nextMonth}
+                className="selectDate-arrows"
+                src={angleRight}
+                alt="angle right"
+              />
+            )}
+          </div>
+        </div>
       </div>
-      <div
-        className={`selectDate-popup ${isCalendarOpen ? "visible" : "hidden"}`}
-      >
-        <p>April</p>
-      </div>
-    </div>
+    </>
   );
 };
 
