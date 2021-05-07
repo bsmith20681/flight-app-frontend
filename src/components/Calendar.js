5e58e1c0c561b46b24d36b626ef5e7944a98a8b3
Ben <bsmith20681@gmail.com>
2021-05-06 20:10:45 -0600

    added alignement for calendar days

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,6 +8,15 @@ const Calendar = () => {
   const [year, setYear] = useState(moment().format("YYYY"));
   const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
 
+  const currentDayOfMonth = moment().date();
+  const currentMonth = moment().month();
+
+  const firstWeekDayOfMonth = (monthNumber) => {
+    return moment(`${monthNumber + 1}-${year}`, "MM-YYYY")
+      .startOf("month")
+      .weekday();
+  };
+
   const daysInMonth = (monthNumber) => {
     return moment(`${year}-${monthNumber}`, "YYYY-MM").daysInMonth();
   };
@@ -23,11 +32,11 @@ const Calendar = () => {
   return (
     <div className="calendar">
       {month < moment().month() + 1 ? (
-        <img className="selectDate-arrows" src={angleLeft} alt="angle left" />
+        <img className="calendar-arrows" src={angleLeft} alt="angle left" />
       ) : (
         <img
           onClick={prevMonth}
-          className="selectDate-arrows"
+          className="calendar-arrows"
           src={angleLeft}
           alt="angle left"
         />
@@ -45,9 +54,17 @@ const Calendar = () => {
           })}
         </div>
         <div className="calendar-days">
-          {[...Array(daysInMonth(month + 1)).keys()].map((x) => (
-            <p className="calendar-day">{x + 1}</p>
-          ))}
+          {console.log(firstWeekDayOfMonth(month))}
+          {[...Array(firstWeekDayOfMonth(month)).keys()].map((x) =>
+            x < firstWeekDayOfMonth(month) ? <div></div> : null
+          )}
+          {[...Array(daysInMonth(month + 1)).keys()].map((x) =>
+            x < currentDayOfMonth - 1 && month === currentMonth ? (
+              <p className="calendar-day_unactive">{x + 1}</p>
+            ) : (
+              <p className="calendar-day">{x + 1}</p>
+            )
+          )}
         </div>
       </div>
       <div className="calendar-single">
@@ -65,17 +82,20 @@ const Calendar = () => {
           })}
         </div>
         <div className="calendar-days">
+          {[...Array(firstWeekDayOfMonth(month + 1)).keys()].map((x) =>
+            x < firstWeekDayOfMonth(month + 1) ? <div></div> : null
+          )}
           {[...Array(daysInMonth(month + 2)).keys()].map((x) => (
             <p className="calendar-day">{x + 1}</p>
           ))}
         </div>
       </div>
       {month >= 10 ? (
-        <img className="selectDate-arrows" src={angleRight} alt="angle right" />
+        <img className="calendar-arrows" src={angleRight} alt="angle right" />
       ) : (
         <img
           onClick={nextMonth}
-          className="selectDate-arrows"
+          className="calendar-arrows"
           src={angleRight}
           alt="angle right"
         />
